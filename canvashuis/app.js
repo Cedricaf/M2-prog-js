1a2d5d69e3d65edbac39ed7f98de8cbca8bf6516
Cedricaf <34737@ma-web.nl>
2022-11-25 10:03:13 +0100

    100x100

--- canvashuis/app.js
+++ canvashuis/app.js
@@ -7,33 +7,44 @@ class App
         let g = canvas.getContext("2d");
         g.beginPath();
         g.fillStyle = "red";
-        g.moveTo(300,100);
-        g.lineTo(700,200);
-        g.lineTo(600,400);
-        g.lineTo(200,300);
-        g.lineTo(300,100);
+        g.moveTo(30,10);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.lineTo(30,10);
         g.fill();
         g.closePath();
+
         g.beginPath();
         g.fillStyle ="grey";
-        g.lineTo(700,200);
-        g.lineTo(800,300);
-        g.lineTo(800,500);
-        g.lineTo(600,600);
-        g.lineTo(600,400);
-        g.lineTo(200,300);
-        g.lineTo(200,500);
-        g.lineTo(600,600);
-        g.lineTo(600,400);
-        g.lineTo(800,300);
-        g.lineTo(700,200);
-        g.lineTo(600,400);
-        g.lineTo(800,300);
+        g.lineTo(70,20);
+        g.lineTo(80,30);
+        g.lineTo(80,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
         g.fill();
         g.stroke();
         g.closePath();
+
         g.beginPath();
-        g.lineTo(800,200)
+        g.fillStyle ="lightblue";
+        g.moveTo(35,35);
+        g.lineTo(35,45);
+        g.lineTo(45,47.5);
+        g.lineTo(45,38);
+        g.moveTo(35,35);
+        g.lineTo(45,38);
+        g.fill();
+        g.stroke();
+        g.closePath();
         
         
         console.log(canvas);
