dda68cd5ada7aae367fc6da5710ca6bcc8b2d119
Cedricaf <34737@ma-web.nl>
2022-11-29 12:07:24 +0100

    new stuff

---
+++ ifelse/app.js
@@ -0,0 +1,62 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello World!");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        g.beginPath();
+        g.fillStyle = "red";
+        g.moveTo(30,10);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.lineTo(30,10);
+        g.fill();
+        g.closePath();
+
+        g.beginPath();
+        g.fillStyle ="grey";
+        g.lineTo(70,20);
+        g.lineTo(80,30);
+        g.lineTo(80,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
+        g.fill();
+        g.stroke();
+        g.closePath();
+
+        g.beginPath();
+        g.fillStyle ="lightblue";
+        g.moveTo(35,35);
+        g.lineTo(35,45);
+        g.lineTo(45,47.5);
+        g.lineTo(45,38);
+        g.moveTo(35,35);
+        g.lineTo(45,38);
+        g.fill();
+        g.stroke();
+        g.closePath();
+        
+        let title = document.getElementById("newstitle");
+        let randomgetal = Math.random();
+        title.style.backgroundColor ="#eec900";
+
+        
+        console.log(randomgetal);
+        console.log(title);
+        console.log(canvas);
+    }
+}
+
+let app = new App();
+app.runApplication();
+
