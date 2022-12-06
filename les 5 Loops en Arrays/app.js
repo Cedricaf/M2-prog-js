8f841ee081d4e93aee2f7b7c053d9b0c4685830b
Cedricaf <34737@ma-web.nl>
2022-12-06 12:50:23 +0100

    new files

---
+++ les 5 Loops en Arrays/app.js
@@ -0,0 +1,18 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let list = ["Bruno Mars", "Rihanna", "The Weekend", "Michael Jackson", "Dua Lipa", "Sia"]
+        let Headers = document.getElementsByClassName("bandName")
+        for(let i = 0; i < Headers.length;  i++){
+            Headers[i].innerHTML = list[i];
+        }
+        console.log(list)
+    }
+    
+
+}
+
+let app = new App();
+app.runApplication();
